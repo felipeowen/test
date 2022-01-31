582fec3695947a6e2e8282e8ea1e5feccf3840d9
felipeowen <phillipray1@gmail.com>
2022-01-30 18:49:21 -0600

    cleaning up now that the Chiefs lost

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+import axios from 'axios';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    people: [],
+  },
+  getters: {
+    people: state => state.people,
+  },
+  mutations: {
+    setPeople(state, val) {
+      state.people = val;
+    },
+  },
+  actions: {
+    async getPeople({ commit }) {
+      await axios.get('https://swapi.dev/api/people').then((res) => {
+        commit('setPeople', res.data.results);
+      });
+    },
+  },
   modules: {},
 });
--- src/views/People.vue
+++ src/views/People.vue
@@ -1,28 +1,37 @@
 <template>
-  <v-list>
-    <v-list-item v-for="(person, index) in people" :key="index">{{
-      person.name
-    }}</v-list-item>
-  </v-list>
+  <v-container fluid grid-list-md>
+    <v-row>
+      <v-col v-for="(person, index) in people" :key="index" class="pa-2" md="3">
+        <v-card>
+          <v-card-title class="text-h5"> {{ person.name }}</v-card-title>
+          <v-list>
+            <v-list-item>
+              <v-list-item-title>Birth Year</v-list-item-title>
+              <v-list-item-subtitle class="text-right">
+                {{ person.birth_year }}
+              </v-list-item-subtitle>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
-import axios from 'axios';
 export default {
   name: 'People',
-  data() {
-    return {
-      people: null,
-    };
+  computed: {
+    people() {
+      return this.$store.state.people;
+    },
   },
   methods: {
-    getPeople() {
-      axios.get('https://swapi.dev/api/people').then((res) => {
-        this.people = res.data.results;
-      });
+    species(species) {
+      return species.length === 0 ? 'N/A' : species[0];
     },
   },
   mounted() {
-    this.getPeople();
+    this.$store.dispatch('getPeople');
   },
 };
 </script>
